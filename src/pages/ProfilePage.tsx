e6dcbafccc20722e239ed1b7a0655c3dea558dda
Wojo3214 <55576529+Wojo3214@users.noreply.github.com>
2023-06-16 13:18:53 +0200

    added Profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import './ProfilePage.css';
 
-const Tab3: React.FC = () => {
+const ProfilePage: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Profile</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 3 page" />
@@ -23,4 +23,4 @@ const Tab3: React.FC = () => {
   );
 };
 
-export default Tab3;
+export default ProfilePage;
